7ab1d467dd53c5e70c14df43edf5ecd098c11a53
Juan <juan@videogami.co>
2017-09-05 10:23:26 -0400

    got it to work with numbers

--- addthemup.js
+++ addthemup.js
@@ -8,19 +8,22 @@
 // });
 
 // create a number variable
-
+var sumOfTheNumbers = 0;
 // how many of them are there ?
+
+var howManyParameters = process.argv.length
 
 // for loop
 
-for (counter = 2; counter < process.argv.length; counter++) {
+for (counter = 2; counter < howManyParameters; counter++) {
   console.log("Counter ", counter, " Value ", process.argv[counter])
   // if the parameter is a number ?
-
+  if (!isNaN(process.argv[counter])) {
     //  keep adding to THAT variable
-
-
+    sumOfTheNumbers = sumOfTheNumbers + parseInt(process.argv[counter])
+    console.log("Is a number !")
+  }
 }
 
-
 // return THAT variable
+console.log("Final Result: ", sumOfTheNumbers)
