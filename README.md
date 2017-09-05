a1f862b71cc17c289127bd306e651117a47eb7bd
Juan <juan@videogami.co>
2017-09-05 09:36:14 -0400

    Just getting started with pseudocode

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # sep5-w1d1
 My first lecture for the Sep 4 cohort
+
+We are going to take a number of arguments from the command line and add them up in Node.
---
+++ addthemup.js
@@ -0,0 +1,17 @@
+// It is going to take a bunch of parameters and
+// add them up.
+
+
+// Capture the parameters
+
+// create a number variable
+
+// how many of them are there ?
+
+// for loop
+
+  // if the parameter is a number ?
+
+    //  keep adding to THAT variable
+
+// return THAT variable
