dd90bb05fd664e57d3628a84857b093e35716c7b
Juan <juan@videogami.co>
2017-09-05 10:05:04 -0400

    Got a for loop working

--- addthemup.js
+++ addthemup.js
@@ -1,8 +1,11 @@
 // It is going to take a bunch of parameters and
 // add them up.
 
-
 // Capture the parameters
+
+// process.argv.forEach((val, index) => {
+//   console.log(`${index}: ${val}`);
+// });
 
 // create a number variable
 
@@ -10,8 +13,14 @@
 
 // for loop
 
+for (counter = 2; counter < process.argv.length; counter++) {
+  console.log("Counter ", counter, " Value ", process.argv[counter])
   // if the parameter is a number ?
 
     //  keep adding to THAT variable
+
+
+}
+
 
 // return THAT variable
